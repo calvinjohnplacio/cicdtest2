083e8677ba945ab5d888fd97ecdf7a4b745c4057
calvinjohnplacio <calvinjohn.placio@g.batstate-u.edu.ph>
2026-04-25 11:23:00 +0800

    all files

---
+++ module.php
@@ -0,0 +1,7 @@
+<h1 style="BackgroundColor:Black; color:green">
+<?php 
+
+echo "Pogi si jison titum";
+
+?>
+</h1>
---
+++ module3.php
@@ -0,0 +1,6 @@
+
+<body style="Background-color:black">
+<h1 style="color:yellow">
+<?php echo "This is Module 3"; ?>
+</h1>
+</body>
